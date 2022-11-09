5e780b7255863856978d6bb96543b06092ef8388
schenaid cristiano <74442598+scheydder@users.noreply.github.com>
2022-11-09 19:25:21 -0300

    Create editar.js

---
+++ editar.js
@@ -0,0 +1,16 @@
+bioEp.init({
+            html: '<div id="bio_ep_content">' +
+            '<img src="pop-up/imagens-pop/seta.png" width="0" alt="Participe" style="margin-right: 130px;" />' +
+            '<span></span>' +
+            '<span></span>' +
+            '<span></span>' +
+            '<a href="https://www.even3.com.br/datec2022/" target="_blank" class="bio_btn">QUERO ME INSCREVER AGORA &nbsp; <i class="fa fa-instagram"></i></a>' +
+            '</div>',
+            css: '#bio_ep {width: 900px; height: 350px; color: #FFD700; background-color: #FFD700; text-align: center ;background-image: url(pop-up/imagens-pop/DATec2.jpg);background-size: cover;}' +
+            '#bio_ep_content {padding: 255px 0 0 0; font-family: inherit;}' +
+            '#bio_ep_content span:nth-child(2) {display: block; color: #FFD700; font-size: 32px; font-weight: 600;}' +
+            '#bio_ep_content span:nth-child(3) {display: block; color: #FFD700; font-size: 16px;}' +
+            '#bio_ep_content span:nth-child(4) {display: block; margin: -5px 0 0 0; font-size: 16px; font-weight: 600;}' +
+            '.bio_btn {display: inline-block; margin: 18px 0 0 0; padding: 8px 25px 8px 25px; color: #fff; font-size: 19px; font-weight: 900; background-color: #DAA520; border: 1px solid #fff; cursor: pointer; -webkit-appearance: none; -moz-appearance: none; border-radius: 100px; text-decoration: none;}',
+            cookieExp: 0
+        });
